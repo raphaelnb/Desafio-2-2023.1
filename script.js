1bb95c2ad2d1b495ddede97cdad10f890e9d2a4f
Raphael Nobre <nobre.roots@gmail.com>
2023-06-08 17:52:06 -0300

    design finalizado

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const openLinkedin = () => window.open('https://www.linkedin.com/company/engnet-consultoria-s-c/')
+
+const openInsta = () => window.open('https://www.instagram.com/engnetconsultoria/')
+
+const openWhats = () => window.open('https://wa.me/5561995936553/')
+
+const mail = () => window.location.href = 'mailto:contato@engnetconsultoria.com.br'
